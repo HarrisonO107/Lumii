79792ffb4c0ac3ca7b4f37e3f047142bf8c531cf
Harrison Oarton <oartonh@gmail.com>
2026-03-19 21:22:39 +0000

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Nav from "./components/Nav";
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: "lumii — AI Beauty Advisor",
